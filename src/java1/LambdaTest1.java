3cc17c0288586e88c89bf037af44483b05976788
dai <728864511@qq.com>
2019-09-09 17:02:33 +0800

    优化一下

--- src/java1/LambdaTest1.java
+++ src/java1/LambdaTest1.java
@@ -12,18 +12,13 @@ import java.util.function.Consumer;
  * -> :lambda操作符 或 箭头操作符
  * ->左边：lambda形参列表 （其实就是接口中的抽象方法的形参列表）
  * ->右边：lambda体 （其实就是重写的抽象方法的方法体）
- * <p>
  * 3. Lambda表达式的使用：（分为6种情况介绍）
- * <p>
  * 总结：
  * ->左边：lambda形参列表的参数类型可以省略(类型推断)；如果lambda形参列表只有一个参数，其一对()也可以省略
  * ->右边：lambda体应该使用一对{}包裹；如果lambda体只有一条执行语句（可能是return语句），省略这一对{}和return关键字
- * <p>
  * 4.Lambda表达式的本质：作为函数式接口的实例
- * <p>
  * 5. 如果一个接口中，只声明了一个抽象方法，则此接口就称为函数式接口。我们可以在一个接口上使用 @FunctionalInterface 注解，
  * 这样做可以检查它是否是一个函数式接口。
- * <p>
  * 6. 所以以前用匿名实现类表示的现在都可以用Lambda表达式来写。
  */
 public class LambdaTest1 {
@@ -38,36 +33,32 @@ public class LambdaTest1 {
                 System.out.println("语法格式一：无参，无返回值");
             }
         };
-
         r1.run();
-
         System.out.println("***********************");
-
         Runnable r2 = () -> {
             System.out.println("语法格式一：无参，无返回值");
         };
-
         r2.run();
+
     }
 
     /**
      * 语法格式二：Lambda 需要一个参数，但是没有返回值。
+     * 其实就是消费型
      */
     @Test
     public void test2() {
-
         Consumer<String> con = new Consumer<String>() {
             @Override
             public void accept(String s) {
                 System.out.println(s);
             }
         };
-        con.accept("语法格式二：Lambda 需要一个参数，但是没有返回值。");
-
+        con.accept("我是入参");
         System.out.println("*******************");
         //如果只有一段实现代码,{}也可以不要
         Consumer<String> con1 = (String s) -> System.out.println(s);
-        con1.accept("语法格式二：Lambda 需要一个参数，但是没有返回值。");
+        con1.accept("我是Lambda格式的入参,左边是形参 ->右边是实现,用于消费形参的哈");
 
     }
 
@@ -80,17 +71,16 @@ public class LambdaTest1 {
         Consumer<String> con1 = (String s) -> {
             System.out.println(s);
         };
-        con1.accept("语法格式三：数据类型可以省略，因为可由编译器推断得出，称为“类型推断”");
-
+        con1.accept("数据类型可以省略,类型推断");
         System.out.println("*******************");
 
         Consumer<String> con2 = System.out::println;
-        con2.accept("语法格式三：数据类型可以省略，因为可由编译器推断得出，称为“类型推断”");
+        con2.accept("::就是方法引用的方式,称为“类型推断”");
 
     }
 
     @Test
-    public void test4() {
+    public void test3A() {
         //类型推断
         ArrayList<String> list = new ArrayList<>();
         //类型推断
@@ -102,28 +92,23 @@ public class LambdaTest1 {
      * 语法格式四：Lambda 若只需要一个参数时，参数的小括号可以省略
      */
     @Test
-    public void test5() {
+    public void test4() {
         Consumer<String> con1 = (s) -> {
             System.out.println(s);
         };
         con1.accept("语法格式四：Lambda 若只需要一个参数时，参数的小括号可以省略");
-
         System.out.println("*******************");
-
         Consumer<String> con2 = s -> {
             System.out.println(s);
         };
         con2.accept("语法格式四：Lambda 若只需要一个参数时，参数的小括号可以省略");
-
-
     }
 
     /**
      * 语法格式五：Lambda 需要两个或以上的参数，多条执行语句，并且可以有返回值
      */
     @Test
-    public void test6() {
-
+    public void test5() {
         Comparator<Integer> com1 = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -132,57 +117,31 @@ public class LambdaTest1 {
                 return o1.compareTo(o2);
             }
         };
-
         System.out.println(com1.compare(12, 21));
-
         System.out.println("*****************************");
         Comparator<Integer> com2 = (o1, o2) -> {
             System.out.println(o1);
             System.out.println(o2);
             return o1.compareTo(o2);
         };
-
         System.out.println(com2.compare(12, 6));
-
-
     }
 
     /**
      * 语法格式六：当 Lambda 体只有一条语句时，return 与大括号若有，都可以省略
      */
     @Test
-    public void test7() {
-
+    public void test6() {
         Comparator<Integer> com1 = (o1, o2) -> {
             return o1.compareTo(o2);
         };
-
         System.out.println(com1.compare(12, 6));
-
         System.out.println("*****************************");
-
+        //可以使用方法应用的方式 Comparator<Integer> com2 = Integer::compareTo;
         Comparator<Integer> com2 = (o1, o2) -> o1.compareTo(o2);
-
         System.out.println(com2.compare(12, 21));
-
     }
 
-    /**
-     * 消费者测试示意
-     */
-    @Test
-    public void test8() {
-        Consumer<String> con1 = s -> {
-            System.out.println(s);
-        };
-        con1.accept("一个是听得人当真了，一个是说的人当真了");
 
-        System.out.println("*****************************");
-
-        Consumer<String> con2 = s -> System.out.println(s);
-
-        con2.accept("一个是听得人当真了，一个是说的人当真了");
-
-    }
 
 }
